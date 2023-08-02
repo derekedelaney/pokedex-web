7a986a613861b9def09feb8d8bbb92f0d47de894
Derek Delaney <derek.e.delaney@gmail.com>
2023-08-02 16:20:05 -0700

    Updates to the details page

--- src/components/pokemon-detail/PokemonDetails.tsx
+++ src/components/pokemon-detail/PokemonDetails.tsx
@@ -1,38 +1,114 @@
-import {useLoaderData} from "react-router-dom";
-import {IPokemon} from "../../models";
-import styled from "styled-components";
-import {POKEMON_TYPE_COLORS} from '../../const';
+import { useLoaderData } from 'react-router-dom';
+import { IPokemon } from '../../models';
+import styled from 'styled-components';
+import { POKEMON_TYPE_COLORS } from '../../const';
 
 const StyledPokemonDetail = styled.div<{ color: string }>`
-  background-color: ${props => props.color};
+  background-color: ${(props) => props.color};
   color: white;
   font-size: 2rem;
   padding: 2rem;
   height: 100vh;
 `;
 
+const TopDetails = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, auto);
+  grid-gap: 1rem;
+  justify-content: flex-end;
+  align-items: center;
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+
+const FavoriteButton = styled.button`
+  all: unset;
+  cursor: pointer;
+  height: 1.5rem;
+`;
+
 const PokemonImage = styled.img`
   margin: 0 auto;
-  display:block;
+  display: block;
+  width: 20rem;
 `;
 
 const Name = styled.div`
   text-transform: capitalize;
+  text-align: center;
 `;
 
 const TypeBox = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
 `;
 
 const Type = styled.div`
-  padding: .75rem;
-  margin: .5rem;
-  background-color: rgba(0, 0, 0, 0.12);
-  border-radius: 2rem;
+  background-color: rgba(0, 0, 0, 0.18);
+  padding: 0.25rem 1.25rem;
+  margin: 0.5rem;
   text-align: center;
+  border-radius: 2rem;
+  font-size: 1rem;
+  text-transform: capitalize;
+`;
+
+const Card = styled.div`
+  box-shadow: 0 0.25rem 0.5rem 0 rgba(0, 0, 0, 0.2),
+    0 0.375rem 1.25rem 0 rgba(0, 0, 0, 0.19);
+  background-color: #ffffff;
+  margin: 2rem;
+  padding: 2rem;
+  border-radius: 1rem;
+  color: #111;
+`;
+
+const CardTable = styled.div<{ columns: number; fillLastRow?: boolean }>`
+  display: grid;
+  grid-template-columns: repeat(${(props) => props.columns - 1}, auto) ${(
+      props
+    ) => (props.fillLastRow ? '1fr' : 'auto')};
+  grid-row-gap: 1rem;
+  grid-column-gap: 2rem;
+  align-items: center;
+`;
+
+const TableHeader = styled.div`
+  margin: 0;
+  font-size: 2rem;
+  justify-self: end;
+`;
+
+const TableLabel = styled.div`
+  color: #666;
   text-transform: capitalize;
   font-size: 1.5rem;
+  justify-self: end;
+`;
+
+const TableValue = styled.div`
+  font-size: 1rem;
+`;
+
+const TableBarFill = styled.div<{ width: number }>`
+  height: 0.5rem;
+  border-radius: 1rem;
+  background-color: #e3e3e3;
+
+  &:after {
+    content: '';
+    display: block;
+    background: orange;
+    width: ${(props) => props.width}%;
+    height: 100%;
+    border-radius: 1rem;
+  }
+`;
+
+const GridColumnSpacer = styled.div<{ start: number; end: number }>`
+  grid-column-start: ${(props) => props.start};
+  grid-column-end: ${(props) => props.end};
 `;
 
 const getColorByPokemonType = (type: string) =>
@@ -40,12 +116,47 @@ const getColorByPokemonType = (type: string) =>
 
 export function PokemonDetail() {
   const data = useLoaderData() as IPokemon;
+  const maxStatsBase = Math.max(...data.stats.map((s) => s.base_stat), 100);
 
-  return <StyledPokemonDetail color={getColorByPokemonType(data.types[0].type.name)}>
-    <PokemonImage src={data.sprites.other["official-artwork"].front_default}/>
-    <div>#{data.id}</div>
-    <Name>Name: {data.name}</Name>
-    <TypeBox>Type: {data.types.map((type) => <Type>{type.type.name}</Type>)}</TypeBox>
-    <div>Stats: {data.stats.map((stat) => <div>{stat.stat.name}: {stat.base_stat}</div>)}</div>
-  </StyledPokemonDetail>
+  return (
+    <StyledPokemonDetail color={getColorByPokemonType(data.types[0].type.name)}>
+      <TopDetails>
+        <FavoriteButton>
+          <span className="material-symbols-outlined">favorite</span>
+        </FavoriteButton>
+        <div>#{data.id}</div>
+      </TopDetails>
+      <PokemonImage
+        src={data.sprites.other['official-artwork'].front_default}
+      />
+      <Name>{data.name}</Name>
+      <TypeBox>
+        {data.types.map((type) => (
+          <Type key={type.type.name}>{type.type.name}</Type>
+        ))}
+      </TypeBox>
+      <Card>
+        <CardTable columns={2}>
+          <TableHeader>Info</TableHeader>
+          <div />
+          <TableLabel>Height</TableLabel>
+          <TableValue>{data.height}</TableValue>
+          <TableLabel>Weight</TableLabel>
+          <TableValue>{data.weight}</TableValue>
+          <div></div>
+        </CardTable>
+      </Card>
+      <Card>
+        <CardTable columns={3} fillLastRow={true}>
+          <TableHeader>Status</TableHeader>
+          <GridColumnSpacer start={2} end={4} />
+          {data.stats.map((stat) => [
+            <TableLabel>{stat.stat.name}</TableLabel>,
+            <TableValue> {stat.base_stat}</TableValue>,
+            <TableBarFill width={(stat.base_stat / maxStatsBase) * 100} />,
+          ])}
+        </CardTable>
+      </Card>
+    </StyledPokemonDetail>
+  );
 }
--- src/components/pokemon-list-tile/PokemonListTile.tsx
+++ src/components/pokemon-list-tile/PokemonListTile.tsx
@@ -1,12 +1,13 @@
-import {Link} from "react-router-dom";
-import styled from "styled-components";
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const StyledPokemonListTile = styled.div<{ color: string }>`
   display: flex;
   border-radius: 1rem;
   padding: 1rem;
-  box-shadow: 0 .25rem .5rem 0 rgba(0, 0, 0, 0.2), 0 .375rem 1.25rem 0 rgba(0, 0, 0, 0.19);
-  background-color: ${props => props.color};
+  box-shadow: 0 0.25rem 0.5rem 0 rgba(0, 0, 0, 0.2),
+    0 0.375rem 1.25rem 0 rgba(0, 0, 0, 0.19);
+  background-color: ${(props) => props.color};
   height: 100%;
   box-sizing: border-box;
   color: white;
@@ -17,8 +18,9 @@ const StyledLink = styled(Link)`
   text-decoration: none;
   border-radius: 1rem;
 
-  &:focus, &:focus-visible {
-    outline: .25rem solid #0065a5;
+  &:focus,
+  &:focus-visible {
+    outline: 0.25rem solid #0065a5;
   }
 `;
 
@@ -27,6 +29,7 @@ const FavoriteButton = styled.button`
   position: absolute;
   top: 1rem;
   right: 1rem;
+  cursor: pointer;
 `;
 
 const InfoGroup = styled.div`
@@ -52,8 +55,8 @@ const StyledTypes = styled.div`
 
 const TypeChip = styled.div`
   background-color: rgba(0, 0, 0, 0.18);
-  padding: .25rem 1.25rem;
-  margin-right: .5rem;
+  padding: 0.25rem 1.25rem;
+  margin-right: 0.5rem;
   text-align: center;
   border-radius: 2rem;
   font-size: 1rem;
@@ -74,24 +77,30 @@ type PokemonListTileProps = {
   id: number;
 };
 
-export function PokemonListTile({name, imageUrl, backgroundColor, types, id}: PokemonListTileProps) {
-  return <StyledLink to={`pokemon/${id}`} tabIndex={id}>
-    <StyledPokemonListTile color={backgroundColor}>
-      <InfoGroup>
-        <FavoriteButton>
-          <span className="material-symbols-outlined">
-            favorite
-          </span>
-        </FavoriteButton>
-        <StyledNumber>#{id}</StyledNumber>
-        <StyledName>{name}</StyledName>
-        <StyledTypes>
-          {types.map((type) => {
-            return <TypeChip>{type}</TypeChip>;
-          })}
-        </StyledTypes>
-      </InfoGroup>
-      <PokemonImage src={imageUrl} alt={`${name} image}`}/>
-    </StyledPokemonListTile>
-  </StyledLink>
+export function PokemonListTile({
+  name,
+  imageUrl,
+  backgroundColor,
+  types,
+  id,
+}: PokemonListTileProps) {
+  return (
+    <StyledLink to={`pokemon/${id}`} tabIndex={id}>
+      <StyledPokemonListTile color={backgroundColor}>
+        <InfoGroup>
+          <FavoriteButton>
+            <span className="material-symbols-outlined">favorite</span>
+          </FavoriteButton>
+          <StyledNumber>#{id}</StyledNumber>
+          <StyledName>{name}</StyledName>
+          <StyledTypes>
+            {types.map((type) => {
+              return <TypeChip>{type}</TypeChip>;
+            })}
+          </StyledTypes>
+        </InfoGroup>
+        <PokemonImage src={imageUrl} alt={`${name} image}`} />
+      </StyledPokemonListTile>
+    </StyledLink>
+  );
 }
